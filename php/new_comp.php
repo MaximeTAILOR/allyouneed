b0569d35e8a964855567a6ef275a03524e2f3556
Remisln <remistperso@gmail.com>
2021-06-04 15:49:58 +0200

    ajout de la date quand l'on créer un candidat ou une entreprise

--- php/new_comp.php
+++ php/new_comp.php
@@ -4,9 +4,9 @@ session_start();
 
 include './conn_db.php';
 
-if(isset($_POST['prenom'], $_POST['nom'], $_POST['telephone'], $_POST['poste'], $_POST['mail'], $_POST['siret'], $_POST['siren'], $_POST['ape'], $_POST['nomEntreprise'], $_POST['mdp'])){ //on vérifie que tous le éléments sont rensignés
-    
-    $sql = "SELECT * from contact where email_contact='" . $_POST['mail'] . "'"; 
+if (isset($_POST['prenom'], $_POST['nom'], $_POST['telephone'], $_POST['poste'], $_POST['mail'], $_POST['siret'], $_POST['siren'], $_POST['ape'], $_POST['nomEntreprise'], $_POST['mdp'])) { //on vérifie que tous le éléments sont rensignés
+
+    $sql = "SELECT * from contact where email_contact='" . $_POST['mail'] . "'";
     $resultat = mysqli_query($conn, $sql);
     if ($resultat == FALSE) { // S'il y a une erreur dans la requête sql
         $table = array(
@@ -24,9 +24,9 @@ if(isset($_POST['prenom'], $_POST['nom'], $_POST['telephone'], $_POST['poste'], 
         echo $table_encode;
     } elseif (mysqli_num_rows($resultat) == 0) { //S'il n'y a pas la meme addresse mail dans la database
 
-//--------------------------------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------------------------------------------------------------------------------------
 
-        $sql = "SELECT * from administrateur where email_admin='" . $_POST['mail'] . "'"; 
+        $sql = "SELECT * from administrateur where email_admin='" . $_POST['mail'] . "'";
         $resultat = mysqli_query($conn, $sql);
         if ($resultat == FALSE) { // S'il y a une erreur dans la requête sql
             $table = array(
@@ -44,9 +44,9 @@ if(isset($_POST['prenom'], $_POST['nom'], $_POST['telephone'], $_POST['poste'], 
             echo $table_encode;
         } elseif (mysqli_num_rows($resultat) == 0) { //S'il n'y a pas la meme addresse mail dans la database
 
-//----------------------------------------------------------------------------------------------------------------------------------
+            //----------------------------------------------------------------------------------------------------------------------------------
 
-            $sql = "SELECT * from customer where email_customer='" . $_POST['mail'] . "'"; 
+            $sql = "SELECT * from customer where email_customer='" . $_POST['mail'] . "'";
             $resultat = mysqli_query($conn, $sql);
             if ($resultat == FALSE) { // S'il y a une erreur dans la requête sql
                 $table = array(
@@ -64,7 +64,7 @@ if(isset($_POST['prenom'], $_POST['nom'], $_POST['telephone'], $_POST['poste'], 
                 echo $table_encode;
             } elseif (mysqli_num_rows($resultat) == 0) { //S'il n'y a pas la meme addresse mail dans la database
 
-//-------------------------------------------------------------------------------------------------------------------------------------
+                //-------------------------------------------------------------------------------------------------------------------------------------
 
                 $nom = htmlspecialchars($_POST['nom']);
                 $prenom = htmlspecialchars($_POST['prenom']);
@@ -75,35 +75,37 @@ if(isset($_POST['prenom'], $_POST['nom'], $_POST['telephone'], $_POST['poste'], 
                 $siren = htmlspecialchars($_POST['siren']);
                 $ape = htmlspecialchars($_POST['ape']);
                 $entreprise = htmlspecialchars($_POST['nomEntreprise']);
+                $date = date_create($_GET['date']);
+                $date = date_format($date, 'Y-m-d');
                 $mdp = htmlspecialchars($_POST['mdp']);
                 $pass_hash = password_hash($mdp, PASSWORD_DEFAULT);
 
-                $sql = "INSERT INTO company (siret_company, siren_company, ape_company, name_company) values ('" . $siret . "', '" . $siren . "', '" . $ape . "', '" . $entreprise . "')";
+                $sql = "INSERT INTO company (siret_company, siren_company, ape_company, name_company, date_company) values ('" . $siret . "', '" . $siren . "', '" . $ape . "', '" . $entreprise . "', '" . $date . "')";
                 $sql2 = "INSERT INTO contact (siret_company, name_contact, fname_contact, num_contact, job_contact, email_contact, password_contact) values ($siret, '" . $nom . "', '" . $prenom . "', '" . $tel . "', '" . $poste . "', '" . $mail . "', '" . $pass_hash . "')";
                 $resultat = mysqli_query($conn, $sql);
                 $resultat2 = mysqli_query($conn, $sql2);
-                if($resultat == FALSE){
+                if ($resultat == FALSE) {
                     $table = array(
                         'error'  => true,
                         'message' => 'Erreur d\'execution de la requête' . $sql,
                     );
-                    
+
                     $table_encode = json_encode($table);
                     echo $table_encode;
-                }elseif($resultat2 == FALSE){
+                } elseif ($resultat2 == FALSE) {
                     $table = array(
                         'error'  => true,
                         'message' => 'Erreur d\'execution de la requête' . $sql,
                     );
-                    
+
                     $table_encode = json_encode($table);
                     echo $table_encode;
-                }else{
+                } else {
                     $table = array(
                         'error'  => false,
                         'message' => 'Inscription réussie !',
                     );
-                    
+
                     $table_encode = json_encode($table);
                     echo $table_encode;
                 }
@@ -111,4 +113,3 @@ if(isset($_POST['prenom'], $_POST['nom'], $_POST['telephone'], $_POST['poste'], 
         }
     }
 }
-?>
