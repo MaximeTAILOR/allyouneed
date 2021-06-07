8fb33b4ff781a57b0aefb56fa8bc8704b2739cdb
Julien Vanhauwaert <julien.vanhauwaert@gmail.com>
2021-06-07 14:52:20 +0200

    Ajout de la requête ajax afficher ainsi que son traitement + correction de l'html

--- script/scripts_fiche_candidat/script_modif_candidat.js
+++ script/scripts_fiche_candidat/script_modif_candidat.js
@@ -2,28 +2,47 @@ const queryString = window.location.search;
 let idUrl = queryString.split('=')[1];
 
 function remplirFiche (data) {
-    for (column of data) {
-        document.querySelector('#' + column).value = column
+    console.log(data);
+    for (champ of Object.keys(data)) {
+        $('#' + champ).val(data[champ]);
     }
 }
 
-console.log(donnees);
+function genreCheck(donnees) {
+    if ($('#homme').is(':checked')) {
+        delete donnees['homme'];
+        delete donnees['femme'];
+        donnees['genre'] = 'homme';
+    }
+    if ($('#femme').is(':checked')) {
+        delete donnees['homme'];
+        delete donnees['femme'];
+        donnees['genre'] = 'femme';
+    }
+}
+
 if (idUrl == undefined) {
     
     $('#envoyer').click((event) => { 
         event.preventDefault();
         
-        let inputs = document.querySelectorAll('input');
+        let inputs = $("input:not(#envoyer)");
 
         let donnees = {};
         for (element of inputs) {
         donnees[element.id] = element.value;
         }
         donnees['action'] = 'ajouter';
+        donnees['typeDeContrat'] = $('#typeDeContrat').val();
+        donnees['compteRenduAgent'] = $('#compteRenduAgent').val();
+        donnees['statutCandidat'] = $('#statutCandidat').val();
+        donnees['avisAgent'] = $('#avisAgent').val();
+
+        genreCheck(donnees);
 
         $.ajax({
             type: 'GET',
-            url: '../php/action_company.php',
+            url: '../php/action_customer.php',
             dataType: 'json',
             data : donnees,
             success: (data) => {
@@ -44,7 +63,7 @@ if (idUrl == undefined) {
 
     $.ajax({
         type: 'GET',
-        url: '../php/action_company.php',
+        url: '../php/action_customer.php',
         dataType: 'json',
         data: {
             idcustomer : idUrl,
@@ -55,13 +74,13 @@ if (idUrl == undefined) {
           if (data.error){
               alert(data.message);
           } else {
-              remplirFiche(data);
-              alert(data.message);
+              console.log('gg');
+              remplirFiche(data[0]);
           }
             
         },
-        error: () => {
-          alert('Erreur !');
+        error: (error) => {
+          alert(error);
         }
     });
     $('#envoyer').click((event) => { 
@@ -79,7 +98,7 @@ if (idUrl == undefined) {
             event.preventDefault();
                 $.ajax({
                     type: 'GET',
-                    url: '../php/action_company.php',
+                    url: '../php/action_customer.php',
                     dataType: 'json',
                     data : donnees,
                     success: (data) => {
